ec24dd39443d4791865f9c1615d29c820d1ac77b
Ameer Shaik <shaikameerbasha24@gmail.com>
2023-02-26 13:33:20 +0530

    API to get source types

--- backend/source_data/urls.py
+++ backend/source_data/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('get_source_types', views.get_source_types),
     path('source_forms', views.SourceFormTemplateList.as_view()),
     path('source_forms/<str:source_type>', views.SourceFormTemplateDetail.as_view()),
     path('sources', views.SourceList.as_view()),
--- backend/source_data/views.py
+++ backend/source_data/views.py
@@ -1,6 +1,14 @@
+from django.http import JsonResponse
 from rest_framework import generics
 from source_data.serializers import SourceFormTemplateSerializer, SourceSerializer
 from source_data.models import SourceFormTemplate, Source
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def get_source_types(request):
+    source_types = dict(SourceFormTemplate.SourceType.choices)
+
+    return JsonResponse(source_types)
 
 class SourceFormTemplateList(generics.CreateAPIView):
     serializer_class = SourceFormTemplateSerializer
