cdeba302a0c3ecc41d12b336b9476f8d00673542
Ameer Shaik <shaikameerbasha24@gmail.com>
2023-02-26 16:43:24 +0530

    complete source component for adding source

--- frontend/src/app/source-data/components/source/source.component.ts
+++ frontend/src/app/source-data/components/source/source.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+import { Source } from "../../interfaces/source";
+import { SourceForm } from "../../interfaces/source-form";
+import { SourceService } from "../../services/source.service";
 
 @Component({
   selector: 'app-source',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SourceComponent implements OnInit {
 
-  constructor() { }
+  sourceTypes: { [key: string]: string; } = {};
+  sourceForm: SourceForm;
+
+  constructor(private sourceService:SourceService) { }
 
   ngOnInit() {
+    this.getSourceTypes();
+  }
+
+  getSourceTypes() {
+    this.sourceService.getSourceTypes().subscribe(
+      (response) => {
+        this.sourceTypes = response;
+        console.log("Fetched source types");
+      },
+      (error) => {
+        console.error("Error in getting source types");
+      }
+    )
+  }
+
+  getSourceFormTemplate(sourceType: string) {
+    this.sourceService.getSourceFormTemplate(sourceType).subscribe(
+      (response) => {
+        this.sourceForm = response;
+        console.log(`Fetched source form template for ${sourceType}`);
+      },
+      (error) => {
+        console.error("Error in getting source form template");
+      }
+    )
+  }
+
+  createSource(sourceDataForm: NgForm) {
+    const sourceDataFormValues = sourceDataForm.value;
+    // console.log(sourceDataFormValues);
+    // return
+
+    let sourceData = {};
+
+    sourceData["source_form_template"] = this.sourceForm.id;
+    sourceData["company_id"] = 1234;
+    sourceData["source_form_data"] = sourceDataFormValues;
+
+    this.sourceService.createSource(sourceData as Source).subscribe(
+      (response) => {
+        console.info("Successfully saved source data");
+      },
+      (error) => {
+        console.error("Error in saving source form data");
+      }
+    )
   }
 
 }
