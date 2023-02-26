ec8929d1b7a309d1dc2f4c301313cba7d73f244b
Ameer Shaik <shaikameerbasha24@gmail.com>
2023-02-26 13:03:20 +0530

    create source component in frontend, make it the default route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+import { SourceComponent } from "./source-data/components/source/source.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/source", pathMatch: "full" },
+  { path: "source", component: SourceComponent, pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
